c9b04b89e5965907b9a1d0bdf4be5a1170e0f798
Ke Yu <38873367+Yuke-hd@users.noreply.github.com>
2026-08-11 22:49:31 +1000

    build(repo): address scaffold validation

--- simulators/d1mini_can_web/src/main.cpp
+++ simulators/d1mini_can_web/src/main.cpp
@@ -1,24 +1,8 @@
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
-
-namespace {
-
-constexpr char kApSsid[] = "CHANGE_ME";
-constexpr char kApPassword[] = "CHANGE_ME";
-ESP8266WebServer server(80);
-
-void handleRoot() {
-  server.send(200, "text/plain", "D1 Mini CAN simulator scaffold; no CAN output enabled.\n");
-}
-
-} // namespace
+#include <Arduino.h>
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(kApSsid, kApPassword);
-  server.on("/", handleRoot);
-  server.begin();
+  Serial.println("D1 Mini simulator scaffold; CAN and network are not initialized.");
 }
 
-void loop() { server.handleClient(); }
+void loop() {}
